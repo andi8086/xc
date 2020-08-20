0d2991f3786ec8357bfa04fadfbe51a0efe79042
Andreas J. Reichel <webmaster@6th-dimension.com>
2020-08-20 22:03:02 +0200

    First usage of ncurses

--- src/main.c
+++ src/main.c
@@ -1,7 +1,29 @@
 #include <stdlib.h>
+#include <string.h>
 #include <xc.h>
 
 int main(int argc, char *argv[])
 {
+	const char *msg = "Press any key to quit";
+
+
+	initscr();
+	cbreak();
+	noecho();
+
+	clear();
+
+	int x, y;
+
+	x = (COLS - strlen(msg)) / 2 - 1;
+	y = LINES / 2 - 1;
+	mvaddstr(y, x, msg);
+
+	refresh();
+
+	getch();
+
+	endwin();
+
 	return EXIT_SUCCESS;
 }
