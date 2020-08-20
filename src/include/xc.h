f9900d0afceb123210a35076952e4c163bec59a2
Andreas J. Reichel <webmaster@6th-dimension.com>
2020-08-20 21:52:21 +0200

    Include curses headers according to found config

---
+++ src/include/xc.h
@@ -0,0 +1,14 @@
+#ifndef __XC_H__
+#define __XC_H__
+
+#include <config.h>
+
+#ifdef HAVE_NCURSES_H
+#include <ncurses.h>
+#elif defined(HAVE_NCURSESW_H)
+#include <ncursesw.h>
+#elif defined(HAVE_NCURSES_CURSES_H)
+#include <ncurses/curses.h>
+#endif
+
+#endif
