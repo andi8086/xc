53a606b5a543c68207a87f06b5cf041483d338ea
Andreas J. Reichel <webmaster@6th-dimension.com>
2020-08-20 22:11:32 +0200

    Add a window

--- src/main.c
+++ src/main.c
@@ -1,17 +1,41 @@
 #include <stdlib.h>
 #include <string.h>
 #include <xc.h>
+	
+
+WINDOW *g_window;
+
+
+WINDOW *win_create(int height, int width, int y, int x)
+{
+	WINDOW *win;
+	win = newwin(height, width, y, x);
+	box(win, 0, 0);
+	wrefresh(win);
+	return win;
+}
+
+
+void win_destroy(WINDOW *win)
+{
+	delwin(win);
+	refresh();
+}
+
+
 
 int main(int argc, char *argv[])
 {
 	const char *msg = "Press any key to quit";
-
 
 	initscr();
 	cbreak();
 	noecho();
 
 	clear();
+	refresh();
+
+	g_window = win_create(LINES-1, COLS-1, 0, 0);
 
 	int x, y;
 
@@ -19,10 +43,10 @@ int main(int argc, char *argv[])
 	y = LINES / 2 - 1;
 	mvaddstr(y, x, msg);
 
-	refresh();
 
 	getch();
 
+	win_destroy(g_window);
 	endwin();
 
 	return EXIT_SUCCESS;
